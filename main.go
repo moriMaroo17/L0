5813bb7caafefe1d1c89a5c4ecfabcf5285540d5
moriMaroo17 <morimaroo17@gmail.com>
2022-05-23 18:23:43 +0300

    created listening function for NATS-streaming server

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
 
+	stan "github.com/nats-io/stan.go"
+)
+
+func main() {
+	Listen()
+}
+
+func Listen() error {
+	sc, err := stan.Connect("test-cluster", "r9", stan.NatsURL("nats://localhost:4223"))
+	if err != nil {
+		return fmt.Errorf("error connecting: %v", err)
+	}
+	var payment Payment
+	sub, err := sc.Subscribe("foo",
+		func(m *stan.Msg) {
+			fmt.Printf("Got message. Data: %v\n", string(m.Data))
+			err := json.Unmarshal(m.Data, &payment)
+			if err != nil {
+				fmt.Printf("Error unmarsh, wrong data in subscription. Error: %v\n", err)
+			}
+		},
+		stan.StartWithLastReceived())
+	defer sub.Unsubscribe()
+	if err != nil {
+		return fmt.Errorf("error subscribing: %v", err)
+	}
+	return nil
 }
