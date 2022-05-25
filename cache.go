1ce47c4a7d6404734c7a8a659e2ebe840b57f14a
grigory <morimaroo17@gmail.com>
2022-05-25 12:08:26 +0300

    added thread safity for cache sturct

--- cache.go
+++ cache.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Cache struct {
+	locker      sync.RWMutex
 	memoryCache map[string]*Data
 }
 
@@ -11,13 +15,17 @@ func NewCache() Cache {
 }
 
 func (c *Cache) Put(key string, value Data) {
+	c.locker.Lock()
 	c.memoryCache[key] = &value
+	c.locker.Unlock()
 }
 
 func (c *Cache) Get(key string) (Data, error) {
+	c.locker.RLock()
 	data, ok := c.memoryCache[key]
 	if !ok {
 		return Data{}, fmt.Errorf("no such cache key %s", key)
 	}
+	c.locker.RUnlock()
 	return *data, nil
 }
