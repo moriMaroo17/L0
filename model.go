55d7121f44d17daf96c5d2a1876e45d6db524d04
moriMaroo17 <morimaroo17@gmail.com>
2022-05-26 17:06:33 +0300

    created full model for data

--- model.go
+++ model.go
@@ -1,7 +1,17 @@
 package main
 
 type Data struct {
-	Payment Payment
+	Delivery           Delivery
+	Payment            Payment
+	Items              []Item
+	Locale             string
+	Internal_signature string
+	Customer_id        string
+	Delivery_service   string
+	ShardKey           string
+	Sm_id              int
+	Date_created       string
+	Oof_shard          string
 }
 
 type Payment struct {
@@ -9,10 +19,33 @@ type Payment struct {
 	Request_id    string
 	Currency      string
 	Provider      string
-	Amount        uint
+	Amount        float64
 	Payment_dt    int
 	Bank          string
-	Delivery_cost uint
+	Delivery_cost float64
 	Goods_total   uint
-	Custom_fee    int
+	Custom_fee    float64
+}
+
+type Delivery struct {
+	Name    string
+	Phone   string
+	Zip     string
+	City    string
+	Address string
+	Region  string
+	Email   string
+}
+
+type Item struct {
+	Chrt_id      int
+	Track_number string
+	Price        float64
+	Rid          string
+	Name         string
+	Sale         float64
+	Size         string
+	Total_price  float64
+	Nm_id        int
+	Brand        string
 }
